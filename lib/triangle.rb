672f9f3187505890b8884e9d241d60ecfdad3ec5
Steven McVey <smcvey1210@gmail.com>
2022-12-13 12:49:10 -0800

    commit tree b312a74e6de1c8a3e718b53d655f2f148f78f91d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Triangle
   # write code here
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+  end
+
+  def kind
+
+    if @sides.length != 3 || @sides.any?(&:negative?) || @sides.include?(0) ||@sides.sort[0] + @sides.sort[1] <= @sides.sort[2]
+      raise TriangleError
+    else
+      if @sides.uniq.length == 1
+        puts "#{@sides} equilateral"
+        :equilateral
+      elsif @sides.uniq.length == 2
+        puts "#{@sides} isoceles"
+        :isosceles
+      else
+        puts "#{@sides} scalene"
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "error message"
+    end
+  end
+
 end
